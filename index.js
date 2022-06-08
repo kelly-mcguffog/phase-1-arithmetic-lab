b97025ae69b60e1fe371d702aaf6bc63e9393a79
Kelly McGuffog <kellymcguffog@gmail.com>
2022-06-08 09:29:06 -0400

    commit tree ca5c287dbc9f8ea4900ca32cb994e1cc2c0c74a2

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+
+const num1 = 31;
+const num2 = 2;
+let multiply = num1 * num2;
+
+const random = Math.ceil(Math.random());
+
+const num3 = 29;
+const num4 = 5;
+const mod = num3 % num4;
+
+const max = Math.max(1, 2, 3, 4, 20);
